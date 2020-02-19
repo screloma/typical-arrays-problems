99c1e62b279d034abda73026aba9fac5626280e0
screloma <screloma@gmail.com>
2020-02-19 22:51:15 +0300

    feat: implement solution for the task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
+exports.min = function min(array) {
+    if (array == undefined || !array.length) {
+        return 0;
+    }
+    return array.reduce((acc, value) => (value < acc ? value : acc), array[0]);
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if (array == undefined || !array.length) {
+        return 0;
+    }
+    return array.reduce((acc, value) => (value > acc ? value : acc), array[0]);
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    if (array == undefined || !array.length) {
+        return 0;
+    }
+    return array.reduce((acc, value) => acc + value, 0) / array.length;
+};
